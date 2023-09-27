84e760bfb3c68d7460559974060c99cf6ba59a0f
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-27 14:39:34 +0200

    fix-drop

--- database/migrations/2023_09_27_123032_add_date_to_trains_table.php
+++ database/migrations/2023_09_27_123032_add_date_to_trains_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('trains', function (Blueprint $table) {
-            $table->removeColumn("data_partenza");
+            $table->dropColumn("data_partenza");
         });
     }
 };
