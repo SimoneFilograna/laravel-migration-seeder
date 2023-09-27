bc870c71729b93e229e9f80065cfee43f7c54ce6
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-27 14:34:33 +0200

    add-updatede-table-with-date

---
+++ database/migrations/2023_09_27_121137_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string("azienda", 40);
+            $table->string("stazione_partenza", 50);
+            $table->string("stazione_arrivo", 50);
+            $table->time("orario_partenza");
+            $table->time("orario_arrivo");
+            $table->integer("codice_treno");
+            $table->integer("numero_carrozze");
+            $table->boolean("in_orario");
+            $table->boolean("cancellato");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
